aac2672e228ff77b63659b240350b556ec8a9106
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-15 22:35:29 +0800

    fix: Add event listeners to set color and table sizes

--- designs.js
+++ designs.js
@@ -6,23 +6,38 @@ const colorPicker = document.getElementById('colorPicker');
 const pixelCanvas = document.getElementById('pixelCanvas');
 
 // Select color input
+colorPicker.addEventListener('input', setColor);
+
+function setColor(event) {
+    const color = event.target.value;
+}
 
 // Select size input
 inputHeight.addEventListener('input', setHeight);
 
 function setHeight(event) {
     const height = event.target.value;
+    return height;
 }
+
 inputWidth.addEventListener('input', setWidth);
 
 function setWidth(event) {
     const width = event.target.value;
+    return width;
 }
+
 // When size is submitted by the user, call makeGrid()
+sizePicker.addEventListener('submit', setSize);
+
+function setSize(event) {
+    event.preventDefault();
+    makeGrid();
+}
 
 function makeGrid() {
 
 // Your code goes here!
-
+console.log("this function is called.");
 
 }
