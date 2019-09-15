f2ad6a2cf44141b385fb8bb64d5cb2546ddaa497
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-15 22:14:59 +0800

    fix: Declare variables for the DOM elements the user will interact width

--- designs.js
+++ designs.js
@@ -1,10 +1,28 @@
-// Select color input
-// Select size input
+// Declare Variables
+const sizePicker = document.getElementById('sizePicker');
+const inputHeight = document.getElementById('inputHeight');
+const inputWidth = document.getElementById('inputWidth');
+const colorPicker = document.getElementById('colorPicker');
+const pixelCanvas = document.getElementById('pixelCanvas');
 
+// Select color input
+
+// Select size input
+inputHeight.addEventListener('input', setHeight);
+
+function setHeight(event) {
+    const height = event.target.value;
+}
+inputWidth.addEventListener('input', setWidth);
+
+function setWidth(event) {
+    const width = event.target.value;
+}
 // When size is submitted by the user, call makeGrid()
 
 function makeGrid() {
 
 // Your code goes here!
+
 
 }
