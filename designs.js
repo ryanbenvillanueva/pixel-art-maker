507e6ee41fbe00ea144724099f3ad9cb7f4c352c
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-16 15:34:47 +0800

    fix: Add functionality to clear any previous grid before making a new one

--- designs.js
+++ designs.js
@@ -4,12 +4,12 @@ const inputHeight = document.getElementById('inputHeight');
 const inputWidth = document.getElementById('inputWidth');
 const colorPicker = document.getElementById('colorPicker');
 const pixelCanvas = document.getElementById('pixelCanvas');
+const cell = document.getElementsByClassName('cells');
 
 // Initialize Input variables;
 let color = 'black';
 let height = 1;
 let width = 1;
-let grid = document.getElementsByTagName('tr');
 
 // Select color input
 colorPicker.addEventListener('input', (event) => color = event.target.value);
@@ -19,17 +19,15 @@ inputHeight.addEventListener('input', (event) => height = event.target.value);
 inputWidth.addEventListener('input', (event) => width = event.target.value);
 
 // When size is submitted by the user, call makeGrid()
-sizePicker.addEventListener('submit', setSize);
-
-function setSize(event) {
+sizePicker.addEventListener('submit', function (event) {
     event.preventDefault();
-    makeGrid(height, width);
-}
+    clearGrid();
+    makeGrid();
+});
 
-function makeGrid(height, width) {
+function makeGrid() {
     for (let h = 1; h <= height; h++) {
         let tr = document.createElement('tr');
-        // tr.setAttribute('id', h);
         pixelCanvas.appendChild(tr);
         for (let w = 1; w <= width; w++) {
             let td = document.createElement('td');
@@ -37,3 +35,16 @@ function makeGrid(height, width) {
         }
     }
 }
+
+function clearGrid() {
+    while (pixelCanvas.firstChild) {
+        pixelCanvas.removeChild(pixelCanvas.firstChild);
+    }
+}
+// Add color to cell
+
+// cell.addEventListener('click', function addColor(event) {
+//     console.log(event.target);
+// });
+
+
