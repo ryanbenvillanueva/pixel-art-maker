fe77f2a70b9a140947fc576210e65a355163b025
Ryan Ben S. Villanueva <ryanben.villanueva@gmail.com>
2019-09-16 14:14:00 +0800

    feat: Add functionality to make the grid

--- designs.js
+++ designs.js
@@ -5,39 +5,35 @@ const inputWidth = document.getElementById('inputWidth');
 const colorPicker = document.getElementById('colorPicker');
 const pixelCanvas = document.getElementById('pixelCanvas');
 
-// Select color input
-colorPicker.addEventListener('input', setColor);
+// Initialize Input variables;
+let color = 'black';
+let height = 1;
+let width = 1;
+let grid = document.getElementsByTagName('tr');
 
-function setColor(event) {
-    const color = event.target.value;
-}
+// Select color input
+colorPicker.addEventListener('input', (event) => color = event.target.value);
 
 // Select size input
-inputHeight.addEventListener('input', setHeight);
-
-function setHeight(event) {
-    const height = event.target.value;
-    return height;
-}
-
-inputWidth.addEventListener('input', setWidth);
-
-function setWidth(event) {
-    const width = event.target.value;
-    return width;
-}
+inputHeight.addEventListener('input', (event) => height = event.target.value);
+inputWidth.addEventListener('input', (event) => width = event.target.value);
 
 // When size is submitted by the user, call makeGrid()
 sizePicker.addEventListener('submit', setSize);
 
 function setSize(event) {
     event.preventDefault();
-    makeGrid();
+    makeGrid(height, width);
 }
 
-function makeGrid() {
-
-// Your code goes here!
-console.log("this function is called.");
-
+function makeGrid(height, width) {
+    for (let h = 1; h <= height; h++) {
+        let tr = document.createElement('tr');
+        // tr.setAttribute('id', h);
+        pixelCanvas.appendChild(tr);
+        for (let w = 1; w <= width; w++) {
+            let td = document.createElement('td');
+            tr.appendChild(td);
+        }
+    }
 }
